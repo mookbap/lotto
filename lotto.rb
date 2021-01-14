dc91be968cff5e82d8d76c1656c33f7a7f49c363
mookbap <76688714+mookbap@users.noreply.github.com>
2021-01-14 23:58:31 +0900

    Create lotto.rb
    my first code test

---
+++ lotto.rb
@@ -0,0 +1,14 @@
+    lotto = [1...46]
+
+
+    loop do
+        puts "원하는 게임횟수를 입력해주세요"
+        
+            x= gets.chomp.to_i
+
+            (1) upto(x) do
+                puts "#{lotto.saaple(6).sort"
+
+            end
+
+        end       
